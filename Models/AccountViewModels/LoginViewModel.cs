da2d3f60cbaa11920b28724e2d1c67287809ae37
Dre mf Randaci <drerandaci@gmail.com>
2018-04-30 20:50:33 -0500

    account models

---
+++ Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace number_cruncher.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,8 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace number_cruncher.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public virtual ICollection<Insurance> InsuranceAccounts { get; set; }
+        public virtual ICollection<Mileage> MilageRecords { get; set; }
+        public virtual ICollection<Paycheck> Paychecks { get; set; }
+        public virtual ICollection<Other> OtherRecords { get; set; }
     }
 }
